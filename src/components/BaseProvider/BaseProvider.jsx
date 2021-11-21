be3809d88884efc737932f2018a5b793b2cb1b23
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-11-21 16:26:49 +0200

    need to fix actions

--- src/components/BaseProvider/BaseProvider.jsx
+++ src/components/BaseProvider/BaseProvider.jsx
@@ -1,37 +1,44 @@
 import { createContext, useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCosts, getIncomes } from "../../redux/selectors";
+import {  addCost, addIncome, removeCost, removeIncome  } from "../../redux/actions";
+// console.log(addCost, addIncome, removeCost, removeIncome)
 
 export const BaseContext = createContext();
 
 
 const BaseProvider = ({ children }) => {
   const [activePage, setActivePage] = useState("");
-  const [costs, setCosts] = useState([]);
-  const [incomes, setIncomes] = useState([]);
+  const dispatch = useDispatch();
+  const costs = useSelector(getCosts);
+  const incomes = useSelector(getIncomes);
+
+
 
   const toggleActivePage = (activePage = "") => setActivePage(activePage);
 
   const addTransaction = ({ transaction, transType }) => {
-    transType === "costs" &&
-      setCosts((prevCosts) => [...prevCosts, transaction]);
-    transType === "incomes" &&
-      setIncomes((prevIncomes) => [...prevIncomes, transaction]);
+    transType === "costs" && dispatch(addCost(transaction))
+      // setCosts((prevCosts) => [...prevCosts, transaction]);
+    transType === "incomes" && dispatch(addIncome(transaction));
+      // setIncomes((prevIncomes) => [...prevIncomes, transaction]);
   };
 
-  useEffect(() => {
-    const parsedCosts = JSON.parse(localStorage.getItem("costs"));
-    const parsedIncomes = JSON.parse(localStorage.getItem("incomes"));
-    parsedCosts && setCosts(parsedCosts);
-    parsedIncomes && setIncomes(parsedIncomes);
-    // eslint-disable-next-line
-  }, []);
+  // useEffect(() => {
+  //   const parsedCosts = JSON.parse(localStorage.getItem("costs"));
+  //   const parsedIncomes = JSON.parse(localStorage.getItem("incomes"));
+  //   parsedCosts && setCosts(parsedCosts);
+  //   parsedIncomes && setIncomes(parsedIncomes);
+  //   // eslint-disable-next-line
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem("costs", JSON.stringify(costs));
-  }, [costs]);
+  // useEffect(() => {
+  //   localStorage.setItem("costs", JSON.stringify(costs));
+  // }, [costs]);
 
-  useEffect(() => {
-    localStorage.setItem("incomes", JSON.stringify(incomes));
-  }, [incomes]);
+  // useEffect(() => {
+  //   localStorage.setItem("incomes", JSON.stringify(incomes));
+  // }, [incomes]);
   return (
     <BaseContext.Provider
       value={{ activePage, costs, incomes, toggleActivePage, addTransaction }}
